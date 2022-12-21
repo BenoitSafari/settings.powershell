e3471d49139c23343eadf279da2a306e5eeb2cff
BenoitSafari <88612813+BenoitSafari@users.noreply.github.com>
2022-12-21 21:17:47 +0100

    Update Microsoft.PowerShell_profile.ps1
    Wouhou-Pi-Douh!

--- pwsh_profiles/Microsoft.PowerShell_profile.ps1
+++ pwsh_profiles/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,9 @@
 oh-my-posh init pwsh --config 'C:\Users\grego\.posh\themes\material.json' | Invoke-Expression
 
+# Ensure that PSReadLine is installed => Install-Module PSReadLine -Force
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle ListView
+
 Function ExecChrome
 {
     $chrome = "C:\Program Files\Google\Chrome\Application\chrome.exe"
